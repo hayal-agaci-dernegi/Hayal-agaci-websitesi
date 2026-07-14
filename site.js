ecc43b333c28e198947e2b897b989e02f1e71b04
EnesSaygilier <enessaygilier0627@gmail.com>
2026-07-15 00:55:49 +0300

    son logo bükücü

--- site.js
+++ site.js
@@ -28,7 +28,23 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+<<<<<<< HEAD
+window.addEventListener('wheel', function(e) {
+    const lightbox = document.getElementById('resim-lightbox');
+    if (lightbox && lightbox.style.display === 'flex') return;
+
+    e.preventDefault(); 
+    
+    let scrollAmount = e.deltaY * 0.5; 
+    window.scrollBy({
+        top: scrollAmount,
+        left: 0,
+        behavior: 'smooth' 
+    });
+}, { passive: false });
+=======
 // Wheel event listener kaldırıldı — tarayıcının doğal scroll davranışı kullanılıyor
+>>>>>>> dda5d6456ca1c1b0dba75972eff1d8b1cceca9d2
 
 const slider = document.getElementById('scrollWrapper');
 if (slider) {
@@ -131,6 +147,45 @@ document.addEventListener('dragstart', function(e) {
     }
 });
 
+<<<<<<< HEAD
+// ==========================================
+// AKILLI DUYURU VE AKORDEON SİSTEMİ
+// ==========================================
+document.addEventListener("DOMContentLoaded", function() {
+    const panel = document.getElementById("duyuru-kart-sistemi");
+    const kisaMetin = document.getElementById("kisa-duyuru-metni");
+    const tamMetin = document.getElementById("tam-duyuru-metni");
+    const okSimgesi = document.querySelector(".acilis-oku");
+
+    if(panel && kisaMetin) {
+        fetch("duyuru.txt")
+            .then(res => res.text())
+            .then(data => {
+                const mesaj = data.trim();
+                
+                if (mesaj.length > 0) {
+                    panel.style.display = "block";
+                    tamMetin.textContent = mesaj;
+                    
+                    if (mesaj.length > 60) {
+                        kisaMetin.textContent = mesaj.substring(0, 60) + "...";
+                        if(okSimgesi) okSimgesi.style.display = "block";
+                    } else {
+                        kisaMetin.textContent = mesaj;
+                        if(okSimgesi) okSimgesi.style.display = "none"; 
+                        document.querySelector(".duyuru-akordeon").style.pointerEvents = "none";
+                    }
+                } else {
+                    panel.style.display = "none";
+                }
+            })
+            .catch(() => {
+                panel.style.display = "none";
+            });
+    }
+});
+=======
+>>>>>>> dda5d6456ca1c1b0dba75972eff1d8b1cceca9d2
 
 // ==========================================
 // HAREKETLİ İSTATİSTİK SAYAÇLARI
